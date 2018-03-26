1208df4b379ebac759b83097ac7e9e7e2cff006d
gangsterdoris <fridarankesjo@gmail.com>
2018-03-26 21:05:15 +0200

    DEL 6

--- Grupp8Unity/Assets/Scripts/GameController.cs
+++ Grupp8Unity/Assets/Scripts/GameController.cs
@@ -6,9 +6,12 @@ public class GameController : MonoBehaviour {
 
 	public Text[] buttonList;
 
+	private string playerSide;
+
 	void Awake ()
 	{
 		SetGameControllerReferenceOnButtons();
+		playerSide = "X";
 	}
 
 	void SetGameControllerReferenceOnButtons ()
@@ -20,11 +23,63 @@ public class GameController : MonoBehaviour {
 
 	public string GetPlayerSide ()
 	{
-		return "?";
+		return playerSide;
 	}
 
 	public void EndTurn ()
+	{	if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+		{	
+			GameOver();
+		}
+
+		if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		ChangeSides ();
+	}
+
+	void ChangeSides()
 	{
-		Debug.Log("EndTurn is not implemented!");
+		playerSide = (playerSide == "X") ? "O" : "X";
+	}
+	void GameOver ()
+	{
+		for (int i = 0; i < buttonList.Length; i++)
+		{
+			buttonList[i].GetComponentInParent<Button>().interactable = false;
+
+		}
 	}
 }
